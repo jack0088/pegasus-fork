33d183ba4ecb57438135259cd85843d931b73a45
jack0088 <jack0088@me.com>
2018-05-06 22:13:01 +0200

    added some TODO comments to remind myself of what the next steps for the project are

--- server.lua
+++ server.lua
@@ -11,3 +11,9 @@ server:run(function(request, response)
     -- response:writeFile("template/404.html")
     response:writeDefaultErrorMessage(404)
 end)
+
+-- TODO figure out how redirecting will work
+-- TODO make broadcaster work to broadcast updates/stream data to client without having the client to pull
+-- TODO figure out how html templates could be refreshed with the streamed content (ajax? I want to avoid rendering them on the server)
+-- TODO implement the flatDB database
+-- TODO figure out how text messages and simmilar sensible data could be saved at client pc and not the database to save space and privacy
