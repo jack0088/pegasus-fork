911580410419aa1691fb9de6d806d1744a0f5519
aschaefer <aschaefer@aschaefers-iMac.local>
2018-05-09 16:30:20 +0200

    push

--- rest-api.lua
+++ rest-api.lua
@@ -1,23 +1,12 @@
 local pretty = require "lib.pretty"
 local route = require("icarus.plugin.router")()
 
-route:get("/", function(request, response, params)
-    --print("request\n" .. pretty(request:headers()))
-    response:write("All have gone well. Here are all your headers so far:\n" .. pretty(request:headers()))
-end)
+local i = 0
 
 route:get("/subscribe/:topic", function(request, response, params)
-    if params.topic == "foo" then
-        print("reached :foo")
-        response:addHeader("Foo", "fooHeaderAdded")
-        response:forward("/subscribe/bar")
-    elseif params.topic == "bar" then
-        print("reached :bar")
-        response:addHeader("Bar", "BarHeaderAdded-FOOBAR")
-        response:forward("/")
-    end
-
-    -- TODO copy headers on response.forward?!
+    i = i + 1
+    print(i)
+    response:write(i .. ": All have gone well. Here are all your headers so far:\n", true)
 end)
 
 return route
