3043a76ee9c214654d9c9fb26e1290250a44dff2
jack0088 <jack0088@me.com>
2018-09-28 18:19:32 +0200

    fixed regex expression issue with method_propertyname; fixed bug with getters and setters not receiving the self context

--- test.lua
+++ test.lua
@@ -1,9 +1,18 @@
 
 
 
-function empty_node()
+function node_prototype()
     local registry = {}
     local protected = true -- default property behaviour with existing getter/setter
+
+    function split(property)
+        local prefix = string.lower(string.sub(property, 1, 4))
+        local suffix = string.sub(property, 5)
+        if prefix == "get_" or prefix == "set_" then
+            return string.sub(prefix, 1, -2), suffix
+        end
+        return nil, property
+    end
 
     function pointer(method, object, property)
         if type(method) == "string" and #method > 0 then
@@ -15,11 +24,11 @@ function empty_node()
     function get(object, property)
         local value = registry[pointer(nil, object, property)]
         local getter = registry[pointer("get", object, property)]
-        return getter and getter() or value -- invoke getter or return value
+        return getter and getter(object) or value -- invoke getter or return value
     end
 
     function set(object, property, value)
-        local method, property = string.match(property, "^([gset]*)_?(.+)")
+        local method, property = split(property)
         local id = pointer(method, object, property)
 
         if method and #method > 0 then -- register getter/setter
@@ -28,23 +37,40 @@ function empty_node()
         else
             local setter = registry[pointer("set", object, property)]
             if type(setter) ~= "nil" then
-                setter(value) -- pipe value through setter
+                return setter(object, value) -- pipe value through setter
             end
         end
 
         registry[id] = value -- store value/getter/setter
+        return registry[id]
     end
 
-    return setmetatable({}, {__index = get, __newindex = set})
+    function show(object)
+        return pairs(registry or object)
+    end
+
+    return setmetatable({}, {__index = get, __newindex = set, __pairs = show})
 end
 
 
 
+
+
 function translate(x, y)
-    local node = empty_node()
+    local node = node_prototype()
 
     node.foo = "foob"
     node.bar = function() return "bar" end
+
+    node.tbl = {
+        foobar = "foobar",
+        age = 22,
+        gender = {
+            "male",
+            "female",
+            "trans"
+        }
+    }
 
     function node:get_x()
         return x
@@ -57,6 +83,8 @@ function translate(x, y)
     return node
 end
 
+
+local pretty = require "lib.pretty"
 local test = translate(1, 2)
 
 
@@ -70,5 +98,7 @@ print(test.x)
 -- function test:set_foo() error("is private") end
 -- test.foo = "k"
 -- print(test.foo, test.bar)
+
+print(pretty(test))
 
 -- function test:get_x() return "määehh :D" end -- this sould assert
