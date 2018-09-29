f75e188848095ba54ee82f269af5f6cd9357b1b0
jack0088 <jack0088@me.com>
2018-09-29 15:50:39 +0200

    renamed & swapped functions; added todo description

--- test.lua
+++ test.lua
@@ -11,7 +11,7 @@ function object_prototype(use_strict)
         return nil, property
     end
 
-    function pointer(method, object, property)
+    function addr(method, object, property)
         if type(method) == "string" and #method > 0 then
             return string.format("%s@%s->%s", string.lower(method), object, property)
         end
@@ -19,36 +19,27 @@ function object_prototype(use_strict)
     end
 
     function get(object, property)
-        local value = registry[pointer(nil, object, property)]
-        local getter = registry[pointer("get", object, property)]
+        local value = registry[addr(nil, object, property)]
+        local getter = registry[addr("get", object, property)]
         return getter and getter(object) or value -- invoke getter or return value
     end
 
     function set(object, property, value)
         local method, property = split(property)
-        local id = pointer(method, object, property)
+        local id = addr(method, object, property)
 
         if method then -- register getter/setter
             assert(type(value) == "function", string.format("%ster must be a `function` value", method))
             assert(type(registry[id]) == "nil" or protected == false, string.format("%ster has already been defined", method))
-            assert(type(registry[pointer(nil, object, property)]) == "nil" or protected == false, string.format("can not define %ster as property has already been assigned a value", method))
+            assert(type(registry[addr(nil, object, property)]) == "nil" or protected == false, string.format("can not define %ster as property has already been assigned a value", method))
         else
-            local setter = registry[pointer("set", object, property)]
+            local setter = registry[addr("set", object, property)]
             if type(setter) ~= "nil" then return setter(object, value) end -- pipe value through setter
-            assert(type(registry[pointer("get", object, property)]) == "nil" and type(registry[pointer("set", object, property)]) == "nil" or protected == false, string.format("can not assign value as %ster has already been defined", method))
+            assert(type(registry[addr("get", object, property)]) == "nil" and type(registry[addr("set", object, property)]) == "nil" or protected == false, string.format("can not assign value as %ster has already been defined", method))
         end
 
         registry[id] = value -- store value/getter/setter
         return registry[id]
-    end
-
-    function chain()
-        -- TODO implement chaining of objects (=nodes)
-        -- think about use cases - should it inherit or rather instanciate an forward copies?
-    end
-
-    function run()
-        -- TODO
     end
 
     function show(object)
@@ -56,6 +47,16 @@ function object_prototype(use_strict)
         -- property key names must be listed de-hashed (without method type and special characters)
         -- see http://lua-users.org/wiki/GeneralizedPairsAndIpairs
         return pairs(registry or object)
+    end
+
+    function run(object)
+        -- TODO
+    end
+
+    function chain(parent, child)
+        -- TODO implement chaining of objects (=nodes)
+        -- think about use cases - should it inherit or rather instanciate an forward copies?
+        -- PS: or use the __le (>) metamethod instead of __pow (^)
     end
 
     return setmetatable({}, {__index = get, __newindex = set, __pow = chain, __call = run, __pairs = show})
@@ -118,6 +119,6 @@ end
 
 -- print(test.empty_node.haha)
 
-print(pretty(test, 3))
+print(pretty(test, 6))
 
 -- function test:get_x() return "määehh :D" end -- this sould assert
